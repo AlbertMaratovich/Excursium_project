8864db7ca20e6b69ceed8b850d8e529b17117515
AlbertMaratovich <gimalov.9844@gmail.com>
2025-07-24 19:49:41 +0500

    debug

--- base/base_page.py
+++ base/base_page.py
@@ -32,7 +32,7 @@ class BasePage:
     def scroll_to(self, element):
         with allure.step("Скроллим до элемента"):
             self.actions.scroll_to_element(element).perform()
-            # time.sleep(3)
+            time.sleep(3)
             self.driver.execute_script("window.scrollTo({top: window.scrollY + 500,});")
             # ok self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
             self.wait.until(EC.element_to_be_clickable(element))
