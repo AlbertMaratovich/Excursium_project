aab03f39a7accccb1d1091a816bf5b3a621ce9e2
AlbertMaratovich <gimalov.9844@gmail.com>
2025-07-25 19:24:41 +0500

    fixed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,11 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    options.add_argument("--headless") # need to use after debugging
+    #options.add_argument("--headless") # need to use after debugging
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-dev-shm-usage")# this
-    options.add_argument("--disable-gpu") # this
+    #options.add_argument("--disable-dev-shm-usage")# this
+    #options.add_argument("--disable-gpu") # this
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
