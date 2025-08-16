de0efcfcbfe56aa518896d78cb1b8ac6afa2db74
AlbertMaratovich <gimalov.9844@gmail.com>
2025-08-16 18:52:02 +0500

    add debug code

--- tests/test_excursium.py
+++ tests/test_excursium.py
@@ -68,12 +68,13 @@ class TestUserCases:
 
         with allure.step("Ждем и проверяем открытие окна"):
             order_window = driver.find_element(*excursion_page.order_window)
+            main_page.wait.until(lambda x: "display: block;" in order_window.get_attribute("style"))
             main_page.wait.until(lambda x: main_page.is_visible(order_window))
 
         with (allure.step("Заполняем поля корректными данными")):
             date_field = driver.find_element(*excursion_page.date_field)
-            main_page.wait.until(lambda x: main_page.is_clickable(date_field))
-            time.sleep(1)
+            # main_page.wait.until(lambda x: main_page.is_clickable(date_field))
+            # time.sleep(1)
             date_field.click()
             main_page.wait.until(lambda x: "active" in date_field.get_attribute("class"))
             first_day = driver.find_elements("xpath", "//div/span[@class='flatpickr-day ']")[0]
